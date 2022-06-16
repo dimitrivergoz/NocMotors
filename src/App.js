88804ead46cf5879f7d89470835bf2967f37b202
dimitrivergoz <dimitri.vergoz@edu.devinci.fr>
2022-06-16 17:46:02 +0200

    Resp info sec

--- src/App.js
+++ src/App.js
@@ -156,7 +156,7 @@ function App() {
                     Vous souhaitez acheter un véhicule, nous aidons aussi les passionnées à trouver leurs bonheurs.</h4>
 
                   </p>
-                <button className="bg-white py-3 rounded-lg w-8/12 text-black ring-slate-50 mt-10">
+                <button className="bg-white py-3 rounded-lg w-full text-black ring-slate-50 mt-10">
                     <a href="#contact" className=" bg-white text-gray-150 p-3 px-5 rounded-full">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                             <path strokeLinecap="round" strokeLinejoin="round" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
@@ -164,7 +164,7 @@ function App() {
                           +33 6 81 XX XX XX
                     </a>
                 </button>
-                <button className="bg-white py-3 rounded-lg w-8/12 text-black ring-slate-50 mt-2">
+                <button className="bg-white py-3 rounded-lg w-full text-black ring-slate-50 mt-2">
                     <a href="#contact" className=" bg-white text-gray-150 p-3 px-5 rounded-full">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                             <path strokeLinecap="round" strokeLinejoin="round" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />

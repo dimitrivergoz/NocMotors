6786e8bfee21430df047568f9c03af9f7f21f452
dimitrivergoz <dimitri.vergoz@edu.devinci.fr>
2022-06-20 22:20:11 +0200

    Bounce

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -35,7 +35,7 @@ const HeroSection = () => {
             </Button>
           </HeroBtnWrapper>
           <HeroDivSvg className="mt-10 relative md:bottom-[-10em] inline-block">
-            <a href="#">
+            <a href="#services">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 class="h-12 w-12 animate-bounce inline-block"
